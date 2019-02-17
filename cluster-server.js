108ba022803ba7a20acafe2e52a11d2fe0a7bdc9
Damian Cipolat <damian.cipolat@gmail.com>
2019-02-17 16:27:09 -0300

    add

---
+++ cluster-server.js
@@ -0,0 +1,52 @@
+const cluster = require('cluster');
+const http    = require('http');
+
+//Get process ID.
+const pID = process.pid;
+
+//Set number of children process.
+const nWorkers = 8;
+
+//Set base port.
+const port = 8000;
+
+//Base master process.
+const master = ()=>{
+
+	console.log('Master server created!!');
+	console.log('PID:',pID,' running in PORT:',port);
+
+	//Set end process event.
+  cluster.on('exit', (worker, code, signal)=>{
+    console.log('worker ' + worker.process.pid + ' died');
+  });
+
+  cluster.on('death', function(worker) {
+    console.log('worker ' + worker.pid + ' died');
+  });
+
+  //Create the clusters.
+  for (let i = 0; i < nWorkers; i++)
+    cluster.fork();
+
+}
+
+//Children process.
+const worker = (portNum)=>{
+
+  console.log('Worker created, PID:',process.pid,' PORT:',portNum);
+
+  //Create the server.
+  http.createServer((req, res)=>{
+    res.writeHead(200);
+    res.end('Hello from worker',pID,'\n');
+  }).listen(portNum);
+
+}
+
+//We will create N workers process that will share the port 8000
+//to handle messages over tcp request.
+if (cluster.isMaster)
+	master();
+else
+	worker(port);
